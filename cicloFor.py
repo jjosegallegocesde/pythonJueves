8fb9bacd72296d375c323b4ff6e3a4b6760da28c
308 <308@academicos.cesde.edu.co>
2022-03-03 07:38:27 -0500

    NTJ001-logic python- se crea reto con 2 contadores en el while

---
+++ cicloFor.py
@@ -0,0 +1,10 @@
+#for i in range(1,20,5):
+    #print(i)
+
+#trabajo con ARREGLOS (LISTAS)
+nombres=["juan","carlos","martha"]
+print(nombres)
+
+#for para RECORRER listas en PY
+for nombre in nombres:
+    print(nombre)
